04189d28aa0aeb1a795082256eae8d540dccc981
Manish <bhattacharya.manish7@gmail.com>
2017-06-02 18:18:44 +0530

    CSP validation added

--- easy_test.py
+++ easy_test.py
@@ -69,23 +69,58 @@ def dir_listing():
   print "\n",('*' * 10) + "Testing for Directory listing" + ('*' * 10),"\n"
 
   soup = BeautifulSoup(get_headers.text, 'html.parser')
-
-  if soup.img == None:
-    print "no image"
-  else:
-    image_link = soup.img['src']
-    print "Image is located at ", image_link
-    break_path = image_link.split('/')
-    break_path.pop()
-    dir_link = "/".join(break_path)
-    print "Path of the directory is ", dir_link,"\n"
-    request_dir = requests.get(dir_link)
-    if request_dir.status_code == 403:
-      print "Directory is protected with 403"
+  try:
+    if soup.img == None:
+      print "no image"
     else:
-      print "request history=",request_dir.history
-      print "request statuscode=",request_dir.status_code
-      print "visit the link", dir_link
+      image_link = soup.img['src']
+      print "Image is located at ", image_link
+      break_path = image_link.split('/')
+      break_path.pop()
+      dir_link = "/".join(break_path)
+      print "Path of the directory is ", dir_link,"\n"
+      request_dir = requests.get(dir_link)
+      if request_dir.status_code == 403:
+        print "Directory is protected with 403"
+      else:
+        print "request history=",request_dir.history
+        print "request statuscode=",request_dir.status_code
+        print "visit the link", dir_link
+  except:
+    print "error occured"
+
+def csp_validation():
+  print ('*' * 10) + "Testing CSP validation" + ('*' * 10),"\n"
+  url = 'http://' + site_name
+  headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+  response = requests.get(url, headers=headers)
+  
+  google_bypass = "http://ajax.googleapis.com"
+  cloudfront_bypass = "https://*.cloudfront.net"
+
+
+  try:
+    csp_header1 = response.headers['content-security-policy']
+    processed_csp = csp_header1.split(" ")
+    if (google_bypass or cloudfront_bypass) in processed_csp:
+      print "CSP bypass possible"
+    else:
+      print "valid CSP so far.."
+
+  except KeyError:
+    pass
+
+  try:
+    csp_header2 = response.headers['Content-Security-Policy-Report-Only']
+    processed_csp2 = csp_header2.split(" ")
+    if (google_bypass or cloudfront_bypass) in processed_csp2:
+      print "CSP bypass possible from CSP-report-only"
+    else:
+      print "valid CSP or CSP-report-only implemented"
+
+  except KeyError:
+    pass
 
 
 #functions calls
@@ -94,7 +129,9 @@ clickjacking()
 same_site()
 spf_dmarc()
 admin_page()
+csp_validation()
 dir_listing()
+
 
 
 
